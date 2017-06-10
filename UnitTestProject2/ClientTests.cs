b7c39681580ea0ead1e365c157016f716d74c685
roman <https://github.com/lotockijj/java-courses>
2017-06-10 11:48:01 +0300

    successful test saved :)

--- UnitTestProject2/ClientTests.cs
+++ UnitTestProject2/ClientTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using task1e;
 using System.Collections.Generic;
 
-namespace UnitTestProject2
+namespace task1eTests
 {
     [TestClass]
     public class ClientTests
