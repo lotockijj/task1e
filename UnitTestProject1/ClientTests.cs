8462cb2966c3f99914de9c2448ce50c9cf6fdf5a
roman <https://github.com/lotockijj/java-courses>
2017-06-10 11:19:19 +0300

    adding test package

---
+++ UnitTestProject1/ClientTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
